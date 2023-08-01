626a94126c15b49ab14a65960169f154c177a5da
johan <johan@DESKTOP-NPIK42K>
2023-08-01 14:25:23 -0500

    Añade ocelot

--- TiendaServicios.Api.Gateway/Program.cs
+++ TiendaServicios.Api.Gateway/Program.cs
@@ -1,11 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
@@ -19,5 +23,6 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+await app.UseOcelot();
 
 app.Run();
